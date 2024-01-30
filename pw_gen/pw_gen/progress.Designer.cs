fd0ce68d5874caee9418050e116e07262964b233
Henrisen <56893232+Henrisen@users.noreply.github.com>
2024-01-30 19:20:53 +0100

    v1.0
    First Upload

---
+++ pw_gen/pw_gen/progress.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace pw_gen
+{
+    partial class progress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(-1, -1);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(500, 25);
+            this.progressBar.TabIndex = 0;
+            // 
+            // progress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 23);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(515, 70);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(515, 70);
+            this.Name = "progress";
+            this.Text = "Progress - Password Generator";
+            this.Load += new System.EventHandler(this.progress_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
---
+++ pw_gen/pw_gen/progress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pw_gen
+{
+    public partial class progress : Form
+    {
+        public int max;
+        public int total;
+        public progress()
+        {
+            InitializeComponent();
+        }
+
+        private void progress_Load(object sender, EventArgs e)
+        {
+            progressBar.Maximum = max;
+        }
+
+        public void update()
+        {
+            progressBar.Value = total;
+            progressBar.Maximum = max;
+            progressBar.Update();
+        }
+    }
+}
